f1ee3ac249ce1c3d9fd87edce633eabaec7f177f
unknown <sw8865@naver.com>
2025-02-19 09:07:39 +0900

    글 작성 구현

--- src/main/BoardCreate.js
+++ src/main/BoardCreate.js
@@ -24,11 +24,18 @@ const BoardCreate = () => {
 
         //FormData 객체에 데이터를 추가한다.
         //또한, 자바스크립트 객체를 JSON 타입으로 변환한다.
-        formData.append('board', JSON.stringify({
+        const boardData = JSON.stringify({
             title: boardInput.title,
             content: boardInput.content,
             visibility: boardInput.visibility
-        }));
+        });
+
+        //FormData를 사용하면 브라우저가 자동으로 Content-type를 multipart/form-data로 설정
+        //그렇기에 변환한 board 데이터를 Blob 객체로 FormData에 추가한다.
+        //Blob은 바이너리 형태에서도 큰 객체를 의미하며, 멀티 데이터(이미지, 비디오 등)를 다룰 때 사용
+        //이렇게 하면 JSON 형식을 유지하면서 multipart/form-data로 데이터를 보낼 수 있다.
+        //이로인해 Spring 컨트트롤러에서도 @RequetPart()에 JSON을 받기가 가능하다.
+        formData.append('board', new Blob([boardData], { type: 'application/json'}));
 
         // file이 존재하면 formData에 파일 추가
         if (file) {
@@ -37,15 +44,16 @@ const BoardCreate = () => {
 
         try{
             //requset data
+            //FormData를 사용하면 브라우저가 자동으로 Content-type를 multipart/form-data로 설정
+            //그렇기에 headers에 content-type를 작성하지 않아도된다.
             const res = await axios.post('http://localhost:8080/api/boards', formData, {
                 // 액세스 토큰을 헤더에 포함
                 headers: {
-                   'Content-Type': 'application/json', // 요청 헤더에 multipart/form-data로 설정
                     Authorization: `${token}`,  
                 }
             });
             alert('게시글 작성완료')
-            navigator('/board');
+            navigate('/board');
         } catch (error) {
             console.log(error);
             alert('서버 오류가 발생했습니다.');
